0a4b488c2bf5f7d255ac4bfb691879ee3b54a000
lytree <hi.yangzh@outlook.com>
2024-01-09 17:42:35 +0800

    Mvvm 通用代码提交

---
+++ Infrastructure.Mvvm/Constant/WindowsVariable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Mvvm;
+
+public static class WindowsVariable
+{
+    /// <summary>
+    /// 主窗口高度
+    /// </summary>
+    public static double MainWindowHeight { get; set; }
+    /// <summary>
+    /// 主窗口宽度
+    /// </summary>
+    public static double MainWindowWidth { get; set; }
+}
---
+++ Infrastructure.Mvvm/Events/WindowsChange.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Mvvm;
+
+
+public class HeightChange : ValueChangedMessage<double>
+{
+    public HeightChange(double height) : base(height)
+    {
+    }
+}
+public class WidthChange : ValueChangedMessage<double>
+{
+    public WidthChange(double height) : base(height)
+    {
+    }
+}
