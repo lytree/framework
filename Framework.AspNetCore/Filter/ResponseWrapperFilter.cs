8dd1db30b6579b3661b2f170573a7d01e94466e9
lytree <hi.yangzh@outlook.com>
2024-03-27 13:46:12 +0800

    update version

--- Framework.AspNetCore/Filter/ResponseWrapperFilter.cs
+++ Framework.AspNetCore/Filter/ResponseWrapperFilter.cs
@@ -20,64 +20,64 @@ public class NonFormatResultAttribute : Attribute
 
 
 
-public class ResponseWrapperFilter : IAsyncResultFilter
-{
-    private readonly ILogger<ResponseWrapperFilter> _logger;
+//public class ResponseWrapperFilter : IAsyncResultFilter
+//{
+//    private readonly ILogger<ResponseWrapperFilter> _logger;
 
-    public ResponseWrapperFilter(ILogger<ResponseWrapperFilter> logger)
-    {
-        _logger = logger;
-    }
-    public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
-    {
-        var actionExecutedContext = await next();
+//    public ResponseWrapperFilter(ILogger<ResponseWrapperFilter> logger)
+//    {
+//        _logger = logger;
+//    }
+//    public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+//    {
+//        var actionExecutedContext = await next();
 
-        if (actionExecutedContext.Exception != null)
-        {
-            return;
-        }
+//        if (actionExecutedContext.Exception != null)
+//        {
+//            return;
+//        }
 
-        if (context.ActionDescriptor.EndpointMetadata.Any(m => m.GetType() == typeof(NonFormatResultAttribute)))
-        {
-            return;
-        }
+//        if (context.ActionDescriptor.EndpointMetadata.Any(m => m.GetType() == typeof(NonFormatResultAttribute)))
+//        {
+//            return;
+//        }
 
-        IActionResult result = actionExecutedContext.Result;
+//        IActionResult result = actionExecutedContext.Result;
 
-        var formatResult = result switch
-        {
-            ViewResult => false,
-            PartialViewResult => false,
-            ViewComponentResult => false,
-            PageResult => false,
-            FileResult => false,
-            SignInResult => false,
-            SignOutResult => false,
-            RedirectToPageResult => false,
-            RedirectToRouteResult => false,
-            RedirectResult => false,
-            RedirectToActionResult => false,
-            LocalRedirectResult => false,
-            ChallengeResult => false,
-            ForbidResult => false,
-            BadRequestObjectResult => false,
-            _ => true,
-        };
+//        var formatResult = result switch
+//        {
+//            ViewResult => false,
+//            PartialViewResult => false,
+//            ViewComponentResult => false,
+//            PageResult => false,
+//            FileResult => false,
+//            SignInResult => false,
+//            SignOutResult => false,
+//            RedirectToPageResult => false,
+//            RedirectToRouteResult => false,
+//            RedirectResult => false,
+//            RedirectToActionResult => false,
+//            LocalRedirectResult => false,
+//            ChallengeResult => false,
+//            ForbidResult => false,
+//            BadRequestObjectResult => false,
+//            _ => true,
+//        };
 
-        if (!formatResult)
-        {
-            return;
-        }
+//        if (!formatResult)
+//        {
+//            return;
+//        }
 
-        var data = result switch
-        {
-            ContentResult contentResult => contentResult.Content,
-            ObjectResult objectResult => objectResult.Value,
-            JsonResult jsonResult => jsonResult.Value,
-            _ => null,
-        };
+//        var data = result switch
+//        {
+//            ContentResult contentResult => contentResult.Content,
+//            ObjectResult objectResult => objectResult.Value,
+//            JsonResult jsonResult => jsonResult.Value,
+//            _ => null,
+//        };
 
-        actionExecutedContext.Result = new JsonResult(ApiResponse.Ok<dynamic>(data));
-    }
-}
+//        actionExecutedContext.Result = new JsonResult(ApiResponse.Ok<dynamic>(data));
+//    }
+//}
 
