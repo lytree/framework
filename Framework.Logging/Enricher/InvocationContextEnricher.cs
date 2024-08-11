283c5366e37a1fb1da47b9161e9d50878320c7ee
lytree <hi.yangzh@outlook.com>
2024-08-11 22:52:14 +0800

    update

---
+++ Framework.Logging/Enricher/InvocationContextEnricher.cs
@@ -0,0 +1,44 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Logging.Enricher;
+
+public class InvocationContextEnricher : ILogEventEnricher
+{
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        if (logEvent.Properties.ContainsKey(Constants.LogProperties.SourceContext))
+        {
+            var sourceContext = ((ScalarValue)logEvent.Properties[Constants.LogProperties.SourceContext]).Value?.ToString();
+            var callerFrame = GetCallerStackFrame(sourceContext);
+
+            if (callerFrame != null)
+            {
+                var methodName = callerFrame.GetMethod()?.Name;
+                var lineNumber = callerFrame.GetFileLineNumber();
+                var fileName = callerFrame?.GetFileName();
+
+                logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(Constants.LogProperties.InvocationContextClassName, sourceContext));
+                logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(Constants.LogProperties.InvocationContextMethodName, methodName));
+                logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(Constants.LogProperties.InvocationContextFilePath, fileName));
+                logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(Constants.LogProperties.InvocationContextLineNumber, lineNumber));
+            }
+        }
+    }
+
+    private StackFrame? GetCallerStackFrame(string className)
+    {
+        var trace = new StackTrace(true);
+        var frames = trace.GetFrames();
+
+        var callerFrame = frames.FirstOrDefault(f => f.GetMethod()?.DeclaringType?.FullName == className);
+
+        return callerFrame;
+    }
+}
