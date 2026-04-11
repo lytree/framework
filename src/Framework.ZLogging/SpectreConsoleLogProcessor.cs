b4b25b7220239f73ecbca309a6857ebb594eebfd
lytree <hi.yangzh@outlook.com>
2026-04-11 18:41:28 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch alpha # Your branch is up to date with 'origin/alpha'. # # Changes to be committed: #	modified:   src/Framework.ZLogging/SpectreConsoleLogProcessor.cs #

--- src/Framework.ZLogging/SpectreConsoleLogProcessor.cs
+++ src/Framework.ZLogging/SpectreConsoleLogProcessor.cs
@@ -54,13 +54,45 @@ public class SpectreConsoleProcessor : IAsyncLogProcessor
             return message;
         }
 
-        if (_options.UseTime)
-        {
-            var timestamp = $"[{_options.TraceColor}]{DateTime.Now.ToString(_options.TimeFormat)}[/]";
-            return $"{timestamp} {message}";
-        }
+        var timestamp = DateTime.Now.ToString(_options.TimeFormat);
+        var logLevel = GetLogLevel(message);
+        var levelColor = GetLogLevelAnsiColor(message);
+        var levelStr = $"{levelColor}[{logLevel}][/]";
+        var categoryColor = "\u001b[35m";
+        var category = GetLogCategory(message);
+        var categoryStr = $"{categoryColor}[{category}][/]";
+        return $"{timestamp} {levelStr} {categoryStr} {message}";
+    }
 
-        return message;
+    private string GetLogLevel(string message)
+    {
+        if (message.Contains("Trace")) return "TRACE";
+        if (message.Contains("Debug")) return "DEBUG";
+        if (message.Contains("Information") || message.Contains("Info")) return "INFO";
+        if (message.Contains("Warning") || message.Contains("Warn")) return "WARN";
+        if (message.Contains("Error") || message.Contains("Fail")) return "ERROR";
+        if (message.Contains("Critical") || message.Contains("Fatal")) return "CRITICAL";
+        return "INFO";
+    }
+
+    private string GetLogCategory(string message)
+    {
+        var start = message.IndexOf('[');
+        var end = message.IndexOf(']');
+        if (start >= 0 && end > start)
+        {
+            return message.Substring(start + 1, end - start - 1);
+        }
+        return "";
+    }
+
+    private string GetLogLevelAnsiColor(string message)
+    {
+        if (message.Contains("Trace")) return "\u001b[38;2;200;200;200m";
+        if (message.Contains("Debug")) return "";
+        if (message.Contains("Warning") || message.Contains("Warn")) return "\u001b[33m";
+        if (message.Contains("Error") || message.Contains("Fail") || message.Contains("Critical") || message.Contains("Fatal")) return "\u001b[31m";
+        return "\u001b[36m";
     }
 
     private void Write(string message)
