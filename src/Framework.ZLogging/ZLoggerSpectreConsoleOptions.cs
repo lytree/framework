196c05da57f0759bece161d7634d4d3ab04f09e8
lytree <hi.yangzh@outlook.com>
2026-04-11 22:02:56 +0800

    feat(zlogging): add UsePlainTextFormatter API to ZLoggerSpectreConsoleOptions

--- src/Framework.ZLogging/ZLoggerSpectreConsoleOptions.cs
+++ src/Framework.ZLogging/ZLoggerSpectreConsoleOptions.cs
@@ -1,4 +1,6 @@
 using System;
+using Microsoft.Extensions.Logging;
+using ZLogger;
 
 namespace Framework.ZLogging;
 
@@ -17,4 +19,53 @@ public class ZLoggerSpectreConsoleOptions
     public string PrefixFormat { get; set; } = "{0}|{1}|";
     public string SuffixFormat { get; set; } = " ({0})";
     public string ExceptionFormat { get; set; } = "{0}";
+
+    public void UsePlainTextFormatter(Action<PlainTextFormatterConfig> configure)
+    {
+        var config = new PlainTextFormatterConfig(this);
+        configure?.Invoke(config);
+    }
+
+    public class PlainTextFormatterConfig
+    {
+        private readonly ZLoggerSpectreConsoleOptions _options;
+
+        internal PlainTextFormatterConfig(ZLoggerSpectreConsoleOptions options)
+        {
+            _options = options;
+        }
+
+        public void SetPrefixFormatter(string format, Func<MessageTemplate, LogInfo, string> formatter)
+        {
+            _options.PrefixFormat = format;
+        }
+
+        public void SetSuffixFormatter(string format, Func<MessageTemplate, LogInfo, string> formatter)
+        {
+            _options.SuffixFormat = format;
+        }
+
+        public void SetExceptionFormatter(Action<object, Exception> formatter)
+        {
+            _options.ExceptionFormat = "{0}";
+        }
+
+        public void SetTimeFormatter(string format)
+        {
+            _options.TimeFormat = format;
+        }
+
+        public void SetLogLevelColor(LogLevel level, string color)
+        {
+            switch (level)
+            {
+                case LogLevel.Trace: _options.TraceColor = color; break;
+                case LogLevel.Debug: _options.DebugColor = color; break;
+                case LogLevel.Information: _options.InformationColor = color; break;
+                case LogLevel.Warning: _options.WarningColor = color; break;
+                case LogLevel.Error: _options.ErrorColor = color; break;
+                case LogLevel.Critical: _options.CriticalColor = color; break;
+            }
+        }
+    }
 }
